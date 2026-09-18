4ac19eea2f9e5c381e981d9518fbfdfe108863a4
***** <kn47ytv9mv@privaterelay.appleid.com>
2026-09-18 09:09:17 -0400

    Backend Applied.

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket       = "9d13dd2e-e470-bc3a-00bf-96da556254df"
+    region       = "us-east-1"
+    key          = "terraform.tfstate"
+    use_lockfile = true
+  }
+}
+
+terraform {
   required_version = ">= 1.11"
 }
